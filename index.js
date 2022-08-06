cebe71989514898d5596705b098cdeec00c00126
atharvapatil22 <atharva.22.patil@gmail.com>
2022-08-06 12:46:27 +0530

    added stadiums route

--- index.js
+++ index.js
@@ -41,6 +41,32 @@ app.get("/players", (req, res) => {
   );
 });
 
+app.get("/stadiums", (req, res) => {
+  // let hasMore = false;
+  let { page, limit } = req.query;
+  if (!page) page = 1;
+  if (!limit) limit = 10;
+
+  const offset = page * limit - limit;
+
+  pool.query(
+    "Select * from football_stadiums LIMIT $1 OFFSET $2;",
+    [limit, offset],
+    (err, results) => {
+      if (err) {
+        res.status(400).json({ message: "Some Error Occurred", error: err });
+      } else {
+        // if (results.rows.length % limit === 0) hasMore = true;
+        res.status(200).json({
+          message: "success",
+          // hasMore: hasMore,
+          stadiums: results.rows,
+        });
+      }
+    }
+  );
+});
+
 app.listen(process.env.PORT || port, (error) => {
   if (!error) console.log("Server is running ");
   else console.log("Error occurred, server can't start", error);
